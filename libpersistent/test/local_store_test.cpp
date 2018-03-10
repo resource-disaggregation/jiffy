56330c52ddcc98e77a7fe7e30cf6616ce704a58b
Anurag Khandelwal <anuragk@berkeley.edu>
2018-03-10 14:07:49 -0800

    Added tests for local_store

---
+++ libpersistent/test/local_store_test.cpp
@@ -0,0 +1,50 @@
+#define CATCH_CONFIG_MAIN
+#include <experimental/filesystem>
+#include "catch.hpp"
+#include "../src/local/local_store.h"
+
+using namespace ::elasticmem::persistent;
+TEST_CASE("write_test", "[write]") {
+  local_store store;
+  std::ofstream out("/tmp/a.txt", std::ofstream::out);
+  out << "a";
+  out.close();
+  REQUIRE_NOTHROW(store.write("/tmp/a.txt", "/tmp/b.txt"));
+  std::ifstream in("/tmp/b.txt", std::ifstream::in);
+  std::string data;
+  in >> data;
+  REQUIRE(data == "a");
+  namespace fs = std::experimental::filesystem;
+  fs::path a("/tmp/a.txt");
+  fs::path b("/tmp/b.txt");
+  fs::remove_all(a);
+  fs::remove_all(b);
+}
+
+TEST_CASE("read_test", "[read]") {
+  local_store store;
+  std::ofstream out("/tmp/b.txt", std::ofstream::out);
+  out << "a";
+  out.close();
+  REQUIRE_NOTHROW(store.read("/tmp/b.txt", "/tmp/a.txt"));
+  std::ifstream in("/tmp/a.txt", std::ifstream::in);
+  std::string data;
+  in >> data;
+  REQUIRE(data == "a");
+  namespace fs = std::experimental::filesystem;
+  fs::path a("/tmp/a.txt");
+  fs::path b("/tmp/b.txt");
+  fs::remove_all(a);
+  fs::remove_all(b);
+}
+
+TEST_CASE("remove_test", "[remove]") {
+  local_store store;
+  std::ofstream out("/tmp/a.txt", std::ofstream::out);
+  out << "a";
+  out.close();
+  REQUIRE_NOTHROW(store.remove("/tmp/a.txt"));
+  namespace fs = std::experimental::filesystem;
+  fs::path a("/tmp/a.txt");
+  REQUIRE_FALSE(fs::exists(a));
+}
