af346e9e49a9af46005e4e2301b16eb744359c9c
Anurag Khandelwal <anuragk@berkeley.edu>
2018-03-10 13:29:46 -0800

    Added tests for kv_block

--- libkv/test/kv_block_test.cpp
+++ libkv/test/kv_block_test.cpp
@@ -1,2 +1,80 @@
 #define CATCH_CONFIG_MAIN
+#include <local/local_store.h>
 #include "catch.hpp"
+#include "../src/block/kv_block.h"
+
+using namespace ::elasticmem::kv;
+
+TEST_CASE("put_get_test", "[put][get]") {
+  kv_block block(std::make_shared<local_store>(), "/tmp");
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.put(std::to_string(i), std::to_string(i)));
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE(block.get(std::to_string(i)) == std::to_string(i));
+  }
+  for (std::size_t i = 1000; i < 2000; ++i) {
+    REQUIRE_THROWS_AS(block.get(std::to_string(i)), std::out_of_range);
+  }
+}
+
+TEST_CASE("put_update_get_test", "[put][update][get]") {
+  kv_block block(std::make_shared<local_store>(), "/tmp");
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.put(std::to_string(i), std::to_string(i)));
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE(block.get(std::to_string(i)) == std::to_string(i));
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.update(std::to_string(i), std::to_string(i + 1000)));
+  }
+  for (std::size_t i = 1000; i < 2000; ++i) {
+    REQUIRE_THROWS_AS(block.update(std::to_string(i), std::to_string(i + 1000)), std::out_of_range);
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE(block.get(std::to_string(i)) == std::to_string(i + 1000));
+  }
+}
+
+TEST_CASE("put_remove_get_test", "[put][update][get]") {
+  kv_block block(std::make_shared<local_store>(), "/tmp");
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.put(std::to_string(i), std::to_string(i)));
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE(block.get(std::to_string(i)) == std::to_string(i));
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.remove(std::to_string(i)));
+  }
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_THROWS_AS(block.get(std::to_string(i)), std::out_of_range);
+  }
+}
+
+TEST_CASE("size_test", "[put][size][storage_size][clear]") {
+  kv_block block(std::make_shared<local_store>(), "/tmp");
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.put(std::to_string(i), std::to_string(i)));
+  }
+  REQUIRE(block.size() == 1000);
+  REQUIRE(block.storage_size() == 1000);
+  REQUIRE(block.storage_size() <= block.storage_capacity());
+  REQUIRE_NOTHROW(block.clear());
+  REQUIRE(block.empty());
+}
+
+TEST_CASE("flush_load_test", "[put][flush][clear][load][get]") {
+  kv_block block(std::make_shared<local_store>(), "/tmp");
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE_NOTHROW(block.put(std::to_string(i), std::to_string(i)));
+  }
+  REQUIRE_NOTHROW(block.flush("/test"));
+  REQUIRE_NOTHROW(block.clear());
+  REQUIRE(block.empty());
+  REQUIRE_NOTHROW(block.load("/test"));
+  for (std::size_t i = 0; i < 1000; ++i) {
+    REQUIRE(block.get(std::to_string(i)) == std::to_string(i));
+  }
+}
