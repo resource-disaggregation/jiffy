fb4f7d5236dcdec61a10af3d114c1b3bb693ae8b
Anurag Khandelwal <anuragk@berkeley.edu>
2018-03-09 18:46:17 -0800

    Updated KV RPC interface

--- thrift/kv_service.thrift
+++ thrift/kv_service.thrift
@@ -10,29 +10,29 @@ exception kv_management_rpc_exception {
 }
 
 service kv_rpc_service {
-  void put(1: binary key, 2: binary value)
+  void put(1: i32 block_id, 2: binary key, 3: binary value)
     throws (1: kv_rpc_exception ex),
 
-  binary get(1: binary key)
+  binary get(1: i32 block_id, 2: binary key)
     throws (1: kv_rpc_exception ex),
 
-  binary update(1: binary key, 2: binary value)
+  binary update(1: i32 block_id, 2: binary key, 3: binary value)
     throws (1: kv_rpc_exception ex),
 
-  void remove(1: binary key)
+  void remove(1: i32 block_id, 2: binary key)
     throws (1: kv_rpc_exception ex),
+
+  i64 size(1: i32 block_id)
+      throws (1: kv_management_rpc_exception ex),
 }
 
 service kv_management_rpc_service {
-  void clear()
+  void clear(1: i32 block_id)
     throws (1: kv_management_rpc_exception ex),
 
-  i64 size()
+  i64 storage_capacity(1: i32 block_id)
     throws (1: kv_management_rpc_exception ex),
 
-  i64 capacity()
-    throws (1: kv_management_rpc_exception ex),
-
-  i64 num_entries()
-    throws (1: kv_management_rpc_exception ex),
+  i64 storage_size(1: i32 block_id)
+      throws (1: kv_management_rpc_exception ex),
 }
