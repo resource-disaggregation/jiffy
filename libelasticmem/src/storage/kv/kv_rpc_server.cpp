6b8a975538613bc7458553127296016bc7355157
Anurag Khandelwal <anuragk@berkeley.edu>
2018-03-19 13:46:25 -0700

    Minor fixes

--- libelasticmem/src/storage/kv/kv_rpc_server.cpp
+++ libelasticmem/src/storage/kv/kv_rpc_server.cpp
@@ -17,10 +17,8 @@ using namespace ::apache::thrift::server;
 std::shared_ptr<TThreadedServer> kv_rpc_server::create(std::vector<std::shared_ptr<kv_block>> &blocks,
                                                        const std::string &address,
                                                        int port) {
-  std::shared_ptr<kv_rpc_serviceIfFactory>
-      clone_factory(new kv_rpc_service_factory(blocks));
-  std::shared_ptr<kv_rpc_serviceProcessorFactory>
-      proc_factory(new kv_rpc_serviceProcessorFactory(clone_factory));
+  std::shared_ptr<kv_rpc_serviceIfFactory> clone_factory(new kv_rpc_service_factory(blocks));
+  std::shared_ptr<kv_rpc_serviceProcessorFactory> proc_factory(new kv_rpc_serviceProcessorFactory(clone_factory));
   std::shared_ptr<TServerSocket> sock(new TServerSocket(address, port));
   std::shared_ptr<TBufferedTransportFactory> transport_factory(new TBufferedTransportFactory());
   std::shared_ptr<TBinaryProtocolFactory> protocol_factory(new TBinaryProtocolFactory());
--- libelasticmem/src/storage/kv/kv_rpc_service_factory.cpp
+++ libelasticmem/src/storage/kv/kv_rpc_service_factory.cpp
@@ -14,9 +14,8 @@ using namespace utils;
 kv_rpc_service_factory::kv_rpc_service_factory(std::vector<std::shared_ptr<kv_block>> &blocks) : blocks_(blocks) {}
 
 kv_rpc_serviceIf *kv_rpc_service_factory::getHandler(const TConnectionInfo &conn_info) {
-  std::shared_ptr<TSocket> sock = std::dynamic_pointer_cast<TSocket>(
-      conn_info.transport);
-  LOG(trace) << "[TRACE] Incoming connection from " << sock->getSocketInfo();
+  std::shared_ptr<TSocket> sock = std::dynamic_pointer_cast<TSocket>(conn_info.transport);
+  LOG(trace) << "Incoming connection from " << sock->getSocketInfo();
   return new kv_rpc_service_handler(blocks_);
 }
 
