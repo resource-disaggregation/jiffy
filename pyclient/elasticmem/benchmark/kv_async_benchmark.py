88c217cc74aef91f247950989f30352109908a1f
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-05 19:09:01 -0700

    Use barriers instead of condition variables

--- pyclient/elasticmem/benchmark/kv_async_benchmark.py
+++ pyclient/elasticmem/benchmark/kv_async_benchmark.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from multiprocessing import Process, Condition, Value
+from multiprocessing import Process, Barrier
 
 from elasticmem import ElasticMemClient
 
@@ -15,23 +15,14 @@ def make_workload(path, off, count, client):
     return workload
 
 
-def load_and_run_workload(n_load, load_cv, start_cv, workload_path, workload_off, d_host, d_port, l_port, data_path,
-                          n_ops, n_procs, max_async):
+def load_and_run_workload(barrier, workload_path, workload_off, d_host, d_port, l_port, data_path,
+                          n_ops, max_async):
     client = ElasticMemClient(d_host, d_port, l_port)
     kv = client.open(data_path)
     workload = make_workload(workload_path, workload_off, n_ops, kv)
+    logging.info("[Process] Loaded data for process.")
 
-    with load_cv:
-        n_load.value += 1
-        logging.info("[Process] Loaded data for process.")
-        if n_load.value == n_procs:
-            logging.info("[Process] All processes completed loading, notifying master...")
-            load_cv.notify()
-
-    with start_cv:
-        logging.info("[Process] Waiting for master to start...")
-        start_cv.wait()
-
+    barrier.wait()
     logging.info("[Process] Starting benchmark...")
 
     ops = 0
@@ -52,25 +43,16 @@ def load_and_run_workload(n_load, load_cv, start_cv, workload_path, workload_off
 
 def run_async_kv_benchmark(d_host, d_port, l_port, data_path, workload_path, workload_off=0, n_ops=100000, n_procs=1,
                            max_async=10000):
-    load_cv = Condition()
-    start_cv = Condition()
-    n_load = Value('i', 0)
+    barrier = Barrier(n_procs)
     benchmark = [Process(target=load_and_run_workload,
-                         args=(n_load, load_cv, start_cv, workload_path, workload_off + i * (n_ops / n_procs), d_host,
-                               d_port, l_port, data_path, int(n_ops / n_procs), n_procs, max_async,))
+                         args=(barrier, workload_path, workload_off + i * (n_ops / n_procs), d_host, d_port, l_port,
+                               data_path, int(n_ops / n_procs), max_async,))
                  for i in range(n_procs)]
 
     for b in benchmark:
         b.start()
 
-    logging.info("[Master] Waiting for processes to load data...")
-    with load_cv:
-        load_cv.wait()
-
-    logging.info("[Master] Notifying processes to start...")
-    with start_cv:
-        start_cv.notify_all()
-
     for b in benchmark:
         b.join()
+
     logging.info("[Master] Benchmark complete.")
