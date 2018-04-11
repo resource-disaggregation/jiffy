1abbb96de2b7fb49665f9fc40379c092c80c245d
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-10 18:03:47 -0700

    Added elasticmem package

--- pyclient/setup.py
+++ pyclient/setup.py
@@ -9,7 +9,7 @@ setup(name='elasticmem',
       author_email='anuragk@berkley.edu',
       url='https://www.github.com/ucbrise/elasticmem',
       package_dir={'elasticmem': 'elasticmem'},
-      packages=['elasticmem.kv', 'elasticmem.directory', 'elasticmem.lease'],
+      packages=['elasticmem', 'elasticmem.kv', 'elasticmem.directory', 'elasticmem.lease'],
       setup_requires=['pytest-runner>=2.0,<4.0', 'thrift>=0.11.0'],
       tests_require=['pytest-cov', 'pytest>2.0,<4.0', 'thrift>=0.11.0'],
       install_requires=['thrift>=0.11.0'])
