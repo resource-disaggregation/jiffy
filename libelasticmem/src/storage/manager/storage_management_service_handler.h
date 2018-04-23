b9b9f916a48819250131240475cab685b84bf9c8
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-22 17:37:15 -0700

    Remove invalid includes

--- libelasticmem/src/storage/manager/storage_management_service_handler.h
+++ libelasticmem/src/storage/manager/storage_management_service_handler.h
@@ -4,9 +4,6 @@
 #include "storage_management_service.h"
 #include "../block.h"
 #include "../chain_module.h"
-#include "../../../../../../../../../../usr/local/Cellar/gcc/7.3.0/include/c++/7.3.0/cstdint"
-#include "../../../../../../../../../../usr/local/Cellar/gcc/7.3.0/include/c++/7.3.0/vector"
-#include "../../../../../../../../../../usr/local/Cellar/gcc/7.3.0/include/c++/7.3.0/string"
 
 namespace elasticmem {
 namespace storage {
@@ -30,19 +27,19 @@ class storage_management_service_handler : public storage_management_serviceIf {
                    int32_t chain_role,
                    const std::string &next_block_name) override;
   void slot_range(rpc_slot_range &_return, int32_t block_id) override;
-  void set_exporting(const int32_t block_id,
+  void set_exporting(int32_t block_id,
                      const std::vector<std::string> &target_block,
-                     const int32_t slot_begin,
-                     const int32_t slot_end) override;
-  void set_importing(const int32_t block_id,
+                     int32_t slot_begin,
+                     int32_t slot_end) override;
+  void set_importing(int32_t block_id,
                      const std::string &path,
-                     const int32_t slot_begin,
-                     const int32_t slot_end,
+                     int32_t slot_begin,
+                     int32_t slot_end,
                      const std::vector<std::string> &chain,
-                     const int32_t chain_role,
+                     int32_t chain_role,
                      const std::string &next_block_name) override;
-  void export_slots(const int32_t block_id) override;
-  void set_regular(const int32_t block_id, const int32_t slot_begin, const int32_t slot_end) override;
+  void export_slots(int32_t block_id) override;
+  void set_regular(int32_t block_id, int32_t slot_begin, int32_t slot_end) override;
  private:
   storage_management_exception make_exception(std::exception &e);
   storage_management_exception make_exception(const std::string &msg);
