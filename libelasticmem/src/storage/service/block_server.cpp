4c20d1c34cbad1c847f2ee7a35bc1d3bf388bbbe
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-26 02:19:03 -0700

    Added logging at storage server

--- libelasticmem/src/storage/service/block_server.cpp
+++ libelasticmem/src/storage/service/block_server.cpp
@@ -29,16 +29,20 @@ std::shared_ptr<TServer> block_server::create(std::vector<std::shared_ptr<chain_
       proc_factory(new block_request_serviceProcessorFactory(clone_factory));
   std::shared_ptr<TBinaryProtocolFactory> protocol_factory(new TBinaryProtocolFactory());
   if (non_blocking) {
+    LOG(log_level::info) << "Creating non blocking server with " << num_io_threads << " io threads and "
+                         << num_proc_threads << " processing threads";
     auto thread_manager = ThreadManager::newSimpleThreadManager(static_cast<size_t>(num_proc_threads));
     auto thread_factory = std::make_shared<PosixThreadFactory>();
     thread_manager->threadFactory(thread_factory);
     thread_manager->start();
     std::shared_ptr<TNonblockingServerSocket> socket(new TNonblockingServerSocket(port));
-    std::shared_ptr<TNonblockingServer> server(new TNonblockingServer(proc_factory, protocol_factory, socket, thread_manager));
+    std::shared_ptr<TNonblockingServer>
+        server(new TNonblockingServer(proc_factory, protocol_factory, socket, thread_manager));
     server->setNumIOThreads(static_cast<size_t>(num_io_threads));
     server->setUseHighPriorityIOThreads(true);
     return server;
   } else {
+    LOG(log_level::info) << "Creating threaded server";
     std::shared_ptr<TServerSocket> sock(new TServerSocket(address, port));
     std::shared_ptr<TFramedTransportFactory> transport_factory(new TFramedTransportFactory());
     std::shared_ptr<TServer>
