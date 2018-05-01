bac64c76b8695c7222dff266a9946585fad5bb60
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-30 20:55:53 -0700

    Added logging

--- libelasticmem/src/storage/chain_module.cpp
+++ libelasticmem/src/storage/chain_module.cpp
@@ -1,6 +1,7 @@
 #include "chain_module.h"
 #include "../utils/logger.h"
 #include "kv/kv_block.h"
+#include "../utils/time_utils.h"
 
 namespace elasticmem {
 namespace storage {
@@ -58,10 +59,14 @@ void chain_module::request(sequence_id seq, int32_t oid, const std::vector<std::
     LOG(log_level::error) << "Called accessor operation " << op_name(oid) << " on non-tail node";
     return;
   }
+  auto t1 = time_utils::now_us();
   std::vector<std::string> result;
   run_command(result, oid, args);
+  auto t2 = time_utils::now_us();
+  LOG(log_level::info) << "run_command took " << (t2 - t1) << " us";
   if (is_mutator(oid)) {
     if (!is_tail()) {
+      t1 = time_utils::now_us();
       assert(next_ != nullptr);
       {
         std::lock_guard<std::mutex> lock(request_mtx_); // Ensures FIFO order w.r.t. seq.server_seq_no
@@ -71,10 +76,15 @@ void chain_module::request(sequence_id seq, int32_t oid, const std::vector<std::
         next_->request(seq, oid, args);
       }
       add_pending(seq, oid, args);
+      t2 = time_utils::now_us();
+      LOG(log_level::info) << "forwarding took " << (t2 - t1) << " us";
     } else {
+      t1 = time_utils::now_us();
       clients().respond_client(seq, result);
       subscriptions().notify(op_name(oid), args[0]); // TODO: Fix
       ack(seq);
+      t2 = time_utils::now_us();
+      LOG(log_level::info) << "responding took " << (t2 - t1) << " us";
     }
   } else {
     clients().respond_client(seq, result);
