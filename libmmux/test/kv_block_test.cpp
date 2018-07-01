2d3e609e9c6613577acf0978e0733eba379f775e
Anurag Khandelwal <anuragk@berkeley.edu>
2018-06-30 22:56:18 -0700

    Added dirty bit to optimize flush on kv_block

--- libmmux/test/kv_block_test.cpp
+++ libmmux/test/kv_block_test.cpp
@@ -72,9 +72,14 @@ TEST_CASE("flush_load_test", "[put][flush][reset][load][get]") {
   kv_block block("nil");
   block.slot_range(0, block::SLOT_MAX);
   for (std::size_t i = 0; i < 1000; ++i) {
-    REQUIRE(block.put(std::to_string(i), std::to_string(i)) == "!ok");
+    std::vector<std::string> res;
+    block.run_command(res, kv_op_id::put, {std::to_string(i), std::to_string(i)});
+    REQUIRE(res.front() == "!ok");
   }
-  REQUIRE_NOTHROW(block.flush("local://tmp/test"));
+  REQUIRE(block.is_dirty());
+  REQUIRE(block.flush("local://tmp/test"));
+  REQUIRE(!block.is_dirty());
+  REQUIRE_FALSE(block.flush("local://tmp/test"));
   REQUIRE_NOTHROW(block.reset());
   REQUIRE_NOTHROW(block.slot_range(0, block::SLOT_MAX));
   REQUIRE(block.empty());
