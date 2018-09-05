1fd252b1c848cc4ce1fd1319eb3f5cc5845a2e0e
Anurag Khandelwal <anuragk@berkeley.edu>
2018-09-05 16:37:58 -0700

    Revert to threads without affinity

--- libmmux/src/mmux/storage/service/block_server.cpp
+++ libmmux/src/mmux/storage/service/block_server.cpp
@@ -48,9 +48,7 @@ std::shared_ptr<TServer> block_server::create(std::vector<std::shared_ptr<chain_
     LOG(log_level::info) << "Creating threaded server";
     std::shared_ptr<TServerSocket> sock(new TServerSocket(address, port));
     std::shared_ptr<BufferedTransportFactory> transport_factory(new BufferedTransportFactory(1024 * 1024));
-    stdcxx::shared_ptr<ThreadFactory> thread_factory(new pinned_thread_factory(false));
-    std::shared_ptr<TServer>
-        server(new TThreadedServer(proc_factory, sock, transport_factory, protocol_factory, thread_factory));
+    std::shared_ptr<TServer> server(new TThreadedServer(proc_factory, sock, transport_factory, protocol_factory));
     return server;
   }
 }
