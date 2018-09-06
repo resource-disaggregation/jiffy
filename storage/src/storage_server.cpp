36a40da2ae4038bc520ffab444c0038886880807
Anurag Khandelwal <anuragk@berkeley.edu>
2018-09-05 17:01:37 -0700

    Minor fix

--- storage/src/storage_server.cpp
+++ storage/src/storage_server.cpp
@@ -39,10 +39,7 @@ std::string local_address() {
     temp_addr = interfaces;
     while (temp_addr != NULL) {
       if (temp_addr->ifa_addr->sa_family == AF_INET) {
-        // Check if interface is en0 which is the wifi connection on the iPhone
-        if (strcmp(temp_addr->ifa_name, "en0") == 0) {
-          ip_address = inet_ntoa(((struct sockaddr_in *) temp_addr->ifa_addr)->sin_addr);
-        }
+        ip_address = inet_ntoa(((struct sockaddr_in *) temp_addr->ifa_addr)->sin_addr);
       }
       temp_addr = temp_addr->ifa_next;
     }
