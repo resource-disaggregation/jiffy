c3a4d86b335a7fb2debe371b5817455d29a827a6
Anurag Khandelwal <anuragk@berkeley.edu>
2018-09-16 11:39:22 -0700

    Parameterize timeout, use static variables to store defaults

--- jmmux/core/src/main/java/mmux/MMuxClient.java
+++ jmmux/core/src/main/java/mmux/MMuxClient.java
@@ -15,18 +15,31 @@ import org.apache.thrift.transport.TTransport;
 
 public class MMuxClient implements Closeable {
 
+  private static final String DEFAULT_BACKING_PATH = "local://tmp";
+  private static final int DEFAULT_NUM_BLOCKS = 1;
+  private static final int DEFAULT_CHAIN_LENGTH = 1;
+  private static final int DEFAULT_FLAGS = 0;
+
   private TTransport transport;
   private directory_service.Client fs;
   private LeaseWorker worker;
   private Thread workerThread;
+  private int timeoutMs;
 
   public MMuxClient(String host, int dirPort, int leasePort) throws TException {
-    transport = new TSocket(host, dirPort);
-    fs = new Client(new TBinaryProtocol(transport));
-    transport.open();
-    worker = new LeaseWorker(host, leasePort);
-    workerThread = new Thread(worker);
-    workerThread.start();
+    this(host, dirPort, leasePort, 5000);
+  }
+
+  public MMuxClient(String host, int dirPort, int leasePort, int timeoutMs) throws TException {
+    this.timeoutMs = timeoutMs;
+    TSocket sock = new TSocket(host, dirPort);
+    sock.setTimeout(this.timeoutMs);
+    this.transport = sock;
+    this.fs = new Client(new TBinaryProtocol(transport));
+    this.transport.open();
+    this.worker = new LeaseWorker(host, leasePort);
+    this.workerThread = new Thread(worker);
+    this.workerThread.start();
   }
 
   public directory_service.Client fs() {
@@ -46,42 +59,44 @@ public class MMuxClient implements Closeable {
   }
 
   public KVClient create(String path) throws TException {
-    return create(path, "", 1, 1, 0);
+    return create(path, DEFAULT_BACKING_PATH, DEFAULT_NUM_BLOCKS, DEFAULT_CHAIN_LENGTH,
+        DEFAULT_FLAGS);
   }
 
   public KVClient create(String path, String backingPath) throws TException {
-    return create(path, backingPath, 1, 1, 0);
+    return create(path, backingPath, DEFAULT_NUM_BLOCKS, DEFAULT_CHAIN_LENGTH, DEFAULT_FLAGS);
   }
 
   public KVClient create(String path, String backingPath, int numBlocks, int chainLength)
       throws TException {
-    return create(path, backingPath, numBlocks, chainLength, 0);
+    return create(path, backingPath, numBlocks, chainLength, DEFAULT_FLAGS);
   }
 
   public KVClient create(String path, String backingPath, int numBlocks, int chainLength, int flags)
       throws TException {
     rpc_data_status status = fs.create(path, backingPath, numBlocks, chainLength, flags);
     beginScope(path);
-    return new KVClient(fs, path, status, 1000);
+    return new KVClient(fs, path, status, timeoutMs);
   }
 
   public KVClient open(String path) throws TException {
     rpc_data_status status = fs.open(path);
     beginScope(path);
-    return new KVClient(fs, path, status, 1000);
+    return new KVClient(fs, path, status, timeoutMs);
   }
 
   public KVClient openOrCreate(String path) throws TException {
-    return openOrCreate(path, "", 1, 1, 0);
+    return openOrCreate(path, DEFAULT_BACKING_PATH, DEFAULT_NUM_BLOCKS, DEFAULT_CHAIN_LENGTH,
+        DEFAULT_FLAGS);
   }
 
   public KVClient openOrCreate(String path, String backingPath) throws TException {
-    return openOrCreate(path, backingPath, 1, 1, 0);
+    return openOrCreate(path, backingPath, DEFAULT_NUM_BLOCKS, DEFAULT_CHAIN_LENGTH, DEFAULT_FLAGS);
   }
 
   public KVClient openOrCreate(String path, String backingPath, int numBlocks, int chainLength)
       throws TException {
-    return openOrCreate(path, backingPath, numBlocks, chainLength, 0);
+    return openOrCreate(path, backingPath, numBlocks, chainLength, DEFAULT_FLAGS);
   }
 
   public KVClient openOrCreate(String path, String backingPath, int numBlocks,
@@ -89,7 +104,7 @@ public class MMuxClient implements Closeable {
     rpc_data_status status = fs
         .openOrCreate(path, backingPath, numBlocks, chainLength, flags);
     beginScope(path);
-    return new KVClient(fs, path, status, 1000);
+    return new KVClient(fs, path, status, timeoutMs);
   }
 
   public KVListener listen(String path) throws TException {
