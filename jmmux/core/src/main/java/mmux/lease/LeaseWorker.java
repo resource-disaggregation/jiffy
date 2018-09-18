39ddcde6e2f86c6c2047e7e4d549cfde3a99cb3a
Anurag Khandelwal <anuragk@berkeley.edu>
2018-09-18 11:20:52 -0700

    Handle lease exceptions better

--- jmmux/core/src/main/java/mmux/lease/LeaseWorker.java
+++ jmmux/core/src/main/java/mmux/lease/LeaseWorker.java
@@ -1,32 +1,38 @@
 package mmux.lease;
 
+import java.io.Closeable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import mmux.util.ThriftClientPool;
 import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LeaseWorker implements Runnable {
+public class LeaseWorker implements Runnable, Closeable {
   private Logger logger = LoggerFactory.getLogger(getClass());
 
-  private ThriftClientPool client;
+  private TTransport transport;
+  private lease_service.Client client;
   private List<String> toRenew;
   private AtomicBoolean exit;
   private AtomicLong renewalDurationMs;
 
   public LeaseWorker(String leaseHost, int leasePort) throws TException {
     this.exit = new AtomicBoolean();
-    this.client = ThriftClientPool.build(leaseHost, leasePort);
+    this.transport = new TFramedTransport(new TSocket(leaseHost, leasePort));
+    this.client = new lease_service.Client(new TBinaryProtocol(transport));
     this.toRenew = new ArrayList<>();
     rpc_lease_ack ack = broker().renewLeases(toRenew);
     this.renewalDurationMs = new AtomicLong(ack.getLeasePeriodMs());
   }
 
-  private lease_service.Iface broker() {
-    return client.iface(lease_service.Client.class);
+  private lease_service.Client broker() {
+    return client;
   }
 
   @Override
@@ -44,6 +50,19 @@ public class LeaseWorker implements Runnable {
         long sleepTime = renewalDurationMs.get() - elapsed;
         if (sleepTime > 0) {
           Thread.sleep(sleepTime);
+        }
+      } catch (lease_service_exception e) {
+        String msg = e.getMsg();
+        if (msg != null) {
+          if (msg.startsWith("Path does not exist: ")) {
+            String path = msg.replace("Path does not exist: ", "");
+            logger.info("No longer renewing leases for: " + path);
+            synchronized (this) {
+              toRenew.remove(path);
+            }
+          } else {
+            logger.warn(msg);
+          }
         }
       } catch (Exception e) {
         if (e.getMessage() != null) {
@@ -96,6 +115,13 @@ public class LeaseWorker implements Runnable {
   public void removePaths(String path) {
     synchronized (this) {
       toRenew.removeIf(p -> p.startsWith(path));
+    }
+  }
+
+  @Override
+  public void close() {
+    if (transport != null) {
+      transport.close();
     }
   }
 }
