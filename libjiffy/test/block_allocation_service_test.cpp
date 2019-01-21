96cd8a25312548abbcaeab1085f3511624c6eb20
Anurag Khandelwal <anuragk@berkeley.edu>
2019-01-20 19:42:55 -0800

    Fix test

--- libjiffy/test/block_allocation_service_test.cpp
+++ libjiffy/test/block_allocation_service_test.cpp
@@ -11,7 +11,7 @@ using namespace ::apache::thrift::transport;
 #define HOST "127.0.0.1"
 #define PORT 9090
 
-TEST_CASE("block_allocation_service_test", "[add_replica_to_chain][remove_blocks]") {
+TEST_CASE("block_registration_service_test", "[add_replica_to_chain][remove_blocks]") {
   auto alloc = std::make_shared<dummy_block_allocator>(4);
   auto server = block_registration_server::create(alloc, HOST, PORT);
   std::thread serve_thread([&server] {
@@ -29,7 +29,7 @@ TEST_CASE("block_allocation_service_test", "[add_replica_to_chain][remove_blocks
   REQUIRE(alloc->num_free_blocks() == 4);
   REQUIRE(alloc->num_allocated_blocks() == 0);
   REQUIRE(alloc->num_total_blocks() == 4);
-  REQUIRE_THROWS_AS(allocator.deregister_blocks({"3", "4", "5", "6", "7"}), block_allocation_service_exception);
+  REQUIRE_THROWS_AS(allocator.deregister_blocks({"3", "4", "5", "6", "7"}), block_registration_service_exception);
 
   server->stop();
   if (serve_thread.joinable()) {
