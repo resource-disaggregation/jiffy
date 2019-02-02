69f4aa4c1a8b0746da40d0a67cd8bb25e9531ebc
Anurag Khandelwal <anuragk@berkeley.edu>
2019-02-01 17:56:20 -0800

    Add support for building and testing python client and java client via cmake/make

--- pyjiffy/setup.py
+++ pyjiffy/setup.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import os
+
+jiffy_version = os.getenv('JIFFY_VERSION', 'unstable')
+thrift_version = os.getenv('THRIFT_VERSION', '0.11.0')
 
 setup(name='pyjiffy',
-      version='0.1.0',
+      version=jiffy_version,
       description='Python Client for jiffy',
       author='Anurag Khandelwal',
       author_email='anuragk@berkley.edu',
       url='https://www.github.com/ucbrise/jiffy',
       package_dir={'jiffy': 'jiffy'},
       packages=['jiffy', 'jiffy.storage', 'jiffy.directory', 'jiffy.lease', 'jiffy.notification', 'jiffy.subscription'],
-      setup_requires=['pytest-runner>=2.0,<4.0', 'thrift>=0.11.0'],
-      tests_require=['pytest-cov', 'pytest>2.0,<4.0', 'thrift>=0.11.0'],
-      install_requires=['thrift>=0.11.0'])
+      setup_requires=['pytest-runner>=2.0,<4.0', 'thrift>={}'.format(thrift_version)],
+      tests_require=['pytest-cov', 'pytest>2.0,<4.0', 'thrift>={}'.format(thrift_version)],
+      install_requires=['thrift>={}'.format(thrift_version)])
