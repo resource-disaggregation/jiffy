d04ee7ac1d3e360ceb5f984ed0f3aa6eb8c3c3a6
Yupeng Tang <charles_typ@berkeley.edu>
2019-05-07 00:59:40 -0700

    fix memory allocator

--- libjiffy/src/jiffy/storage/block_memory_manager.cpp
+++ libjiffy/src/jiffy/storage/block_memory_manager.cpp
@@ -1,6 +1,8 @@
 #include <jemalloc/jemalloc.h>
 #include <new>
 #include "block_memory_manager.h"
+#include "jiffy/utils/logger.h"
+using namespace jiffy::utils;
 
 namespace jiffy {
 namespace storage {
@@ -8,16 +10,21 @@ namespace storage {
 block_memory_manager::block_memory_manager(size_t capacity) : capacity_(capacity), used_(0) {}
 
 void *block_memory_manager::mb_malloc(size_t size) {
-  if (used_.load() + size > capacity_) {
+  if (used_.load() > capacity_) {
     return nullptr;
   }
   auto ptr = mallocx(size, 0);
-  used_ += sallocx(ptr, 0);
+  used_ += size;
   return ptr;
 }
 
 void block_memory_manager::mb_free(void *ptr) {
   auto size = sallocx(ptr, 0);
+  free(ptr);
+  used_ -= size;
+}
+
+void block_memory_manager::mb_free(void *ptr, size_t size) {
   free(ptr);
   used_ -= size;
 }
