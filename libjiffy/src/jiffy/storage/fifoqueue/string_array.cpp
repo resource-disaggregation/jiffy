40497e6cb3118690aaca91fd8a36e53093943078
Yupeng Tang (FA Talent) <v-yuptan@microsoft.com>
2019-06-25 11:59:56 +0800

    fix format and add documentation for string array

--- libjiffy/src/jiffy/storage/fifoqueue/string_array.cpp
+++ libjiffy/src/jiffy/storage/fifoqueue/string_array.cpp
@@ -4,15 +4,18 @@
 namespace jiffy {
 namespace storage {
 using namespace utils;
+
 string_array::string_array(std::size_t max_size, block_memory_allocator<char> alloc) : alloc_(alloc), max_(max_size) {
   split_string_ = false;
   data_ = alloc_.allocate(max_);
   tail_ = 0;
   last_element_offset_ = 0;
 }
+
 string_array::~string_array() {
   alloc_.deallocate(data_, max_);
 }
+
 string_array::string_array(const string_array &other) {
   split_string_ = other.split_string_.load();
   alloc_ = other.alloc_;
@@ -21,6 +24,7 @@ string_array::string_array(const string_array &other) {
   tail_ = other.tail_;
   last_element_offset_ = other.last_element_offset_;
 }
+
 string_array &string_array::operator=(const string_array &other) {
   split_string_ = other.split_string_.load();
   alloc_ = other.alloc_;
@@ -30,10 +34,12 @@ string_array &string_array::operator=(const string_array &other) {
   last_element_offset_ = other.last_element_offset_;
   return *this;
 }
+
 bool string_array::operator==(const string_array &other) const {
   return data_ == other.data_ && tail_ == other.tail_ && alloc_ == other.alloc_ && max_ == other.max_
       && last_element_offset_ == other.last_element_offset_;
 }
+
 std::pair<bool, std::string> string_array::push_back(const std::string &msg) {
   std::size_t len = msg.size();
   if (len + tail_ + metadata_length <= max_) {
@@ -55,7 +61,6 @@ std::pair<bool, std::string> string_array::push_back(const std::string &msg) {
     tail_ += remain_len;
     return std::make_pair(false, msg.substr(remain_len, msg.size() - remain_len));
   } else {
-    //throw std::logic_error("This partition is full, should be detected by partition");
     return std::make_pair(false, msg);
   }
 }
@@ -78,6 +83,7 @@ const std::pair<bool, std::string> string_array::at(std::size_t offset) const {
     throw std::logic_error("Invalid offset");
   }
 }
+
 std::size_t string_array::find_next(std::size_t offset) const {
   if (offset >= last_element_offset_) {
     return 0;
@@ -85,35 +91,45 @@ std::size_t string_array::find_next(std::size_t offset) const {
   std::size_t len = *((std::size_t *) (data_ + offset));
   return offset + len + metadata_length;
 }
+
 std::size_t string_array::size() const {
   return tail_;
 }
+
 std::size_t string_array::capacity() {
   return max_ - metadata_length;
 }
+
 bool string_array::split_last_string() {
   return split_string_.load();
 }
+
 void string_array::clear() {
   tail_ = 0;
   last_element_offset_ = 0;
   split_string_ = false;
 }
+
 bool string_array::empty() const {
   return tail_ == 0;
 }
+
 string_array::iterator string_array::begin() {
   return string_array::iterator(*this, 0);
 }
+
 string_array::iterator string_array::end() {
   return string_array::iterator(*this, max_);
 }
+
 std::size_t string_array::max_offset() const {
   return max_;
 }
+
 string_array::const_iterator string_array::begin() const {
   return string_array::const_iterator(*this, 0);
 }
+
 string_array::const_iterator string_array::end() const {
   return string_array::const_iterator(*this, max_);
 }
@@ -158,6 +174,7 @@ const_string_array_iterator::value_type const_string_array_iterator::operator*()
   auto ret = impl_.at(pos_);
   return ret.second;
 }
+
 const const_string_array_iterator const_string_array_iterator::operator++(int) {
   pos_ = impl_.find_next(pos_);
   if (pos_) {
@@ -167,9 +184,11 @@ const const_string_array_iterator const_string_array_iterator::operator++(int) {
     return *this;
   }
 }
+
 bool const_string_array_iterator::operator==(const_string_array_iterator other) const {
   return (impl_ == other.impl_) && (pos_ == other.pos_);
 }
+
 bool const_string_array_iterator::operator!=(const_string_array_iterator other) const {
   return !(*this == other);
 }
