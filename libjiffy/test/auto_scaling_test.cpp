7a42355e5540e6a116f37c4d475cbe4aaaa726bc
Yupeng Tang <charles_typ@berkeley.edu>
2019-09-30 23:44:57 +0800

    Add file auto_scaling tests

--- libjiffy/test/auto_scaling_test.cpp
+++ libjiffy/test/auto_scaling_test.cpp
@@ -1,5 +1,4 @@
 #include "catch.hpp"
-
 #include <thrift/transport/TTransportException.h>
 #include <thread>
 #include <chrono>
@@ -14,6 +13,7 @@
 #include "jiffy/directory/fs/directory_server.h"
 #include "jiffy/storage/client/hash_table_client.h"
 #include "jiffy/storage/client/fifo_queue_client.h"
+#include "jiffy/storage/client/file_client.h"
 #include "jiffy/client/jiffy_client.h"
 #include "jiffy/directory/fs/sync_worker.h"
 #include "jiffy/directory/lease/lease_expiry_worker.h"
@@ -296,40 +296,40 @@ TEST_CASE("file_auto_scale_test", "[directory_service][storage_server][managemen
   test_utils::wait_till_server_ready(HOST, DIRECTORY_SERVICE_PORT);
 
   auto status = t->create("/sandbox/scale_up.txt", "file", "/tmp", 1, 1, 0, perms::all(), {"0"}, {"regular"}, {});
-  file_writer writer(t, "/sandbox/scale_up.txt", status);
-  file_reader reader(t, "/sandbox/scale_up.txt", status);
+  file_client client(t, "/sandbox/scale_up.txt", status);
 
   // Write data until auto scaling is triggered
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE_NOTHROW(writer.write(std::string(512, (std::to_string(i)).c_str()[0])));
+    REQUIRE_NOTHROW(client.write(std::string(512, (std::to_string(i)).c_str()[0])));
   }
 
   for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE_NOTHROW(writer.write(std::string(102400, (std::to_string(i)).c_str()[0])));
+    REQUIRE_NOTHROW(client.write(std::string(102400, (std::to_string(i)).c_str()[0])));
   }
+
+  REQUIRE_NOTHROW(client.seek(0));
+  for (std::size_t i = 0; i < 5000; ++i) {
+    REQUIRE(client.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
+  }
+
+  for (std::size_t i = 0; i < 6000; ++i) {
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+  }
+
+  REQUIRE_NOTHROW(client.seek(0));
 
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE(reader.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
   }
 
   for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
   }
 
-  REQUIRE(reader.seek(0));
-
-  for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE(reader.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
-  }
+  REQUIRE_NOTHROW(client.seek(5000 * 512));
 
   for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
-  }
-
-  REQUIRE(reader.seek(5000 * 512));
-
-  for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
   }
   // Busy wait until number of blocks increases
   while (t->dstatus("/sandbox/scale_up.txt").data_blocks().size() == 1);
@@ -381,37 +381,39 @@ TEST_CASE("file_auto_scale_chain_replica_test", "[directory_service][storage_ser
   test_utils::wait_till_server_ready(HOST, DIRECTORY_SERVICE_PORT);
 
   auto status = t->create("/sandbox/scale_up.txt", "file", "/tmp", 1, 3, 0, perms::all(), {"0"}, {"regular"}, {});
-  file_writer writer(t, "/sandbox/scale_up.txt", status);
-  file_reader reader(t, "/sandbox/scale_up.txt", status);
+  file_client client(t, "/sandbox/scale_up.txt", status);
+
 
 
   // Write data until auto scaling is triggered
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE_NOTHROW(writer.write(std::string(512, (std::to_string(i)).c_str()[0])));
+    REQUIRE_NOTHROW(client.write(std::string(512, (std::to_string(i)).c_str()[0])));
   }
 
   for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE_NOTHROW(writer.write(std::string(102400, (std::to_string(i)).c_str()[0])));
+    REQUIRE_NOTHROW(client.write(std::string(102400, (std::to_string(i)).c_str()[0])));
   }
+
+  REQUIRE_NOTHROW(client.seek(0));
 
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE(reader.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
   }
 
-  REQUIRE_NOTHROW(reader.seek(0));
+  REQUIRE_NOTHROW(client.seek(0));
 
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE(reader.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
   }
 
   for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
   }
 
-  REQUIRE_NOTHROW(reader.seek(5000 * 512));
+  REQUIRE_NOTHROW(client.seek(5000 * 512));
 
   for (std::size_t i = 0; i < 6000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
   }
   // Busy wait until number of blocks increases
   while (t->dstatus("/sandbox/scale_up.txt").data_blocks().size() == 1);
@@ -468,36 +470,37 @@ TEST_CASE("file_auto_scale_multi_blocks_test", "[directory_service][storage_serv
                           {"regular", "regular", "regular", "regular", "regular", "regular", "regular",
                            "regular"},
                           conf);
-  file_writer writer(t, "/sandbox/scale_up.txt", status);
-  file_reader reader(t, "/sandbox/scale_up.txt", status);
+  file_client client(t, "/sandbox/scale_up.txt", status);
 
   // Write data until auto scaling is triggered
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE_NOTHROW(writer.write(std::string(512, (std::to_string(i)).c_str()[0])));
+    REQUIRE_NOTHROW(client.write(std::string(512, (std::to_string(i)).c_str()[0])));
   }
 
   for (std::size_t i = 0; i < 10000; ++i) {
-    REQUIRE_NOTHROW(writer.write(std::string(102400, (std::to_string(i)).c_str()[0])));
+    REQUIRE_NOTHROW(client.write(std::string(102400, (std::to_string(i)).c_str()[0])));
   }
+
+  REQUIRE_NOTHROW(client.seek(0));
 
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE(reader.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
   }
 
-  REQUIRE_NOTHROW(reader.seek(0));
+  REQUIRE_NOTHROW(client.seek(0));
 
   for (std::size_t i = 0; i < 5000; ++i) {
-    REQUIRE(reader.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(512) == std::string(512, (std::to_string(i)).c_str()[0]));
   }
 
   for (std::size_t i = 0; i < 10000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
   }
 
-  REQUIRE_NOTHROW(reader.seek(5000 * 512));
+  REQUIRE_NOTHROW(client.seek(5000 * 512));
 
   for (std::size_t i = 0; i < 10000; ++i) {
-    REQUIRE(reader.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
+    REQUIRE(client.read(102400) == std::string(102400, (std::to_string(i)).c_str()[0]));
   }
 
   // Busy wait until number of blocks increases
