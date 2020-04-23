d7dfeff3c0867a2e59d586fe59831758efabbc9c
akhilraj95 <akhil95raj@gmail.com>
2020-04-23 18:31:12 -0400

    added free pool

---
+++ libjiffy/src/jiffy/maestro/free_block_allocator.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iterator>
+#include "free_block_allocator.h"
+#include "jiffy/storage/manager/detail/block_id_parser.h"
+
+namespace jiffy {
+    namespace directory {
+
+        // TODO: 18/04/20 order the storage servers by free blocks size
+        std::vector<std::string> free_block_allocator::allocate(std::size_t count, const std::vector<std::string> &) {
+            std::unique_lock<std::mutex> lock(mtx_);
+            std::vector<std::string> blocks;
+
+            auto server = free_blocks_by_server_.begin();
+
+            while(blocks.size() < count && server != free_blocks_by_server_.end()) {
+
+                auto free_block_set = server->second;
+                auto free_block_iterator = free_block_set.begin();
+
+                while(blocks.size() < count && free_block_iterator != free_block_set.end()) {
+
+                    blocks.push_back(*free_block_iterator);
+                    free_block_set.erase(free_block_iterator++);
+                }
+
+                server++;
+            }
+
+            return blocks;
+        }
+
+        void free_block_allocator::add_blocks(const std::vector<std::string> &block_names) {
+            std::unique_lock<std::mutex> lock(mtx_);
+
+            for (const auto &block_name: block_names) {
+                auto block = storage::block_id_parser::parse(block_name);
+                auto storage_server_id = block.get_storage_server_id();
+                free_blocks_by_server_[storage_server_id].insert(block_name);
+            }
+        }
+
+        void free_block_allocator::remove_blocks(const std::vector<std::string> &block_names) {
+            std::unique_lock<std::mutex> lock(mtx_);
+            for (const auto &block_name: block_names) {
+                auto block = storage::block_id_parser::parse(block_name);
+                auto storage_server_id = block.get_storage_server_id();
+                free_blocks_by_server_[storage_server_id].insert(block_name);
+            }
+        }
+
+        std::size_t free_block_allocator::num_free_blocks() {
+            std::unique_lock<std::mutex> lock(mtx_);
+
+            unsigned int num_free_blocks = 0;
+
+            for(auto &server: free_blocks_by_server_) {
+                auto free_block_set = server.second;
+                num_free_blocks += free_block_set.size();
+            }
+
+            return num_free_blocks;
+        }
+
+
+    }
+}
---
+++ libjiffy/src/jiffy/maestro/free_block_allocator.h
@@ -0,0 +1,68 @@
+#ifndef JIFFY_FREE_BLOCK_ALLOCATOR_H
+#define JIFFY_FREE_BLOCK_ALLOCATOR_H
+
+
+#include <shared_mutex>
+#include <set>
+#include <unordered_map>
+#include <list>
+#include "jiffy/directory/block/block_allocator.h"
+
+
+namespace jiffy {
+    namespace directory {
+
+        class free_block_allocator : public block_allocator {
+        public:
+            free_block_allocator() = default;
+
+            virtual ~free_block_allocator() = default;
+
+            /**
+             * @brief Allocate blocks in different prefixes
+             * @param count Number of blocks
+             * @return Block names
+             */
+
+            std::vector<std::string> allocate(std::size_t count, const std::vector<std::string> &exclude_list) override;
+
+            /**
+             * @brief Free blocks
+             * @param blocks Block names
+             */
+            void free(const std::vector<std::string> &block_name) override;
+
+            /**
+             * @brief Add blocks to free block list
+             * @param block_names Block names
+             */
+
+            void add_blocks(const std::vector<std::string> &block_names) override;
+
+            /**
+             * @brief Remove blocks from free block list
+             * @param block_names Block names
+             */
+
+            void remove_blocks(const std::vector<std::string> &block_names) override;
+
+            /**
+             * @brief Fetch number of free blocks
+             * @return Number of free blocks
+             */
+
+            std::size_t num_free_blocks() override;
+
+        private:
+
+            /* Operation mutex */
+            std::mutex mtx_;
+            /* Free blocks per storage server*/
+            std::unordered_map<std::string,std::set<std::string>> free_blocks_by_server_;
+
+        };
+
+    }
+}
+
+#endif //JIFFY_FREE_BLOCK_ALLOCATOR_H
