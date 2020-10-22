ef04ec7d05a735128c0ffd8622ca51163b14b4f0
zhangshijie <1791473127@qq.com>
2020-10-22 21:40:36 +0800

    try

--- libjiffy/test/file_client_test.cpp
+++ libjiffy/test/file_client_test.cpp
@@ -86,9 +86,8 @@ TEST_CASE("file_client_write_read_seek_test", "[write][read][seek]") {
   if (mgmt_serve_thread.joinable()) {
     mgmt_serve_thread.join();
   }
-  std::cout << 1 << "\n"; 
   auto err = memkind_destroy_kind(pmem_kind);
-  std::cout << 2 << "\n";
+  std::cout << "err=" << err << "\n";
 }
 
 
