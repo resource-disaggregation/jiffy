77f2276a43cfab48e7d88d5c681e9f65338d0650
zhangshijie <1791473127@qq.com>
2020-10-23 02:26:48 +0800

    t

--- libjiffy/test/local_store_test.cpp
+++ libjiffy/test/local_store_test.cpp
@@ -19,11 +19,8 @@ TEST_CASE("local_write_test", "[write]") {
   std::string memory_mode = "PMEM";
   auto err = memkind_create_pmem(pmem_path.c_str(),0,&pmem_kind);
   size_t capacity = 134217728;
-  std::cout<<"before manager\n";
   block_memory_manager manager(capacity, memory_mode, pmem_kind);
-  std::cout<<"after manager\n";
   block_memory_allocator<uint8_t> binary_allocator(&manager);
-  std::cout<<"after allocator\n";
   hash_table_type table;
   auto bkey = binary("key", binary_allocator);
   auto bval = binary("value", binary_allocator);
