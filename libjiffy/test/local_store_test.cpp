434f1e455be91fe795a83775eeb7cc6b2babd975
zhangshijie <1791473127@qq.com>
2020-10-23 02:28:49 +0800

    t

--- libjiffy/test/local_store_test.cpp
+++ libjiffy/test/local_store_test.cpp
@@ -18,6 +18,7 @@ TEST_CASE("local_write_test", "[write]") {
   std::string pmem_path = "media/pmem0/shijie"; 
   std::string memory_mode = "PMEM";
   auto err = memkind_create_pmem(pmem_path.c_str(),0,&pmem_kind);
+  if (pmem_kind == nullptr) std::cout<<"wocc!!!!!!\n";
   size_t capacity = 134217728;
   block_memory_manager manager(capacity, memory_mode, pmem_kind);
   block_memory_allocator<uint8_t> binary_allocator(&manager);
