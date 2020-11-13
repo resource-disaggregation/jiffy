d406645543d93d6b0e1d14a19492d9cda68ef9f3
zhangshijie <1791473127@qq.com>
2020-11-13 16:54:56 +0800

    cao

--- libjiffy/src/jiffy/storage/shared_log/shared_log_partition.cpp
+++ libjiffy/src/jiffy/storage/shared_log/shared_log_partition.cpp
@@ -201,6 +201,7 @@ void shared_log_partition::get_storage_capacity(response &_return, const arg_lis
 
 void shared_log_partition::run_command(response &_return, const arg_list &args) {
   auto cmd_name = args[0];
+  std::cout<<"cmd_name="<<cmd_name<<"\n";
   switch (command_id(cmd_name)) {
     case shared_log_cmd_id::shared_log_write:write(_return, args);
       break;
