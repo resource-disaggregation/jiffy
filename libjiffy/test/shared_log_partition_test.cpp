27498b0a70084cd55a8b000fbd9d3dec03f39242
zhangshijie <1791473127@qq.com>
2020-11-13 12:23:16 +0800

    t

--- libjiffy/test/shared_log_partition_test.cpp
+++ libjiffy/test/shared_log_partition_test.cpp
@@ -22,13 +22,13 @@ TEST_CASE("shared_log_write_scan_test", "[write][scan]") {
   block_memory_manager manager(capacity, memory_mode, pmem_kind);
   shared_log_partition block(&manager);
   std::size_t offset = 0;
-  for (std::size_t i = 0; i < 10; ++i) {
+  for (std::size_t i = 0; i < 1000; ++i) {
     response resp;
     REQUIRE_NOTHROW(block.write(resp, {"write", std::to_string(i), std::to_string(offset), std::to_string(i)+"_data", std::to_string(i)+"_stream"}));
     REQUIRE(resp[0] == "!ok");
     offset += (12 + 2 * std::to_string(i).size());
   }
-  for (std::size_t start_pos = 0; start_pos < 8; ++start_pos) {
+  for (std::size_t start_pos = 0; start_pos < 998; ++start_pos) {
     response resp;
     REQUIRE_NOTHROW(block.scan(resp, {"scan", std::to_string(start_pos), std::to_string(start_pos + 2), std::to_string(start_pos)+"_stream"}));
     REQUIRE(resp[0] == "!ok");
@@ -51,7 +51,7 @@ TEST_CASE("shared_log_write_trim_scan_test", "[write][read]") {
   block_memory_manager manager(capacity, memory_mode, pmem_kind);
   shared_log_partition block(&manager);
   std::size_t offset = 0;
-  for (std::size_t i = 0; i < 10; ++i) {
+  for (std::size_t i = 0; i < 1000; ++i) {
     response resp;
     REQUIRE_NOTHROW(block.write(resp, {"write", std::to_string(i), std::to_string(offset), std::to_string(i)+"_data", std::to_string(i)+"_stream"}));
     REQUIRE(resp[0] == "!ok");
@@ -72,7 +72,7 @@ TEST_CASE("shared_log_write_trim_scan_test", "[write][read]") {
     REQUIRE(resp.size() == 1);
   }
   
-  for (std::size_t start_pos = 2; start_pos < 8; ++start_pos) {
+  for (std::size_t start_pos = 2; start_pos < 998; ++start_pos) {
     response resp;
     REQUIRE_NOTHROW(block.scan(resp, {"scan", std::to_string(start_pos), std::to_string(start_pos + 2), std::to_string(start_pos)+"_stream"}));
     REQUIRE(resp[0] == "!ok");
---
+++ libjiffy/test/shared_log_performance_test.cpp
@@ -0,0 +1,109 @@
+#include "catch.hpp"
+#include <vector>
+#include <thread>
+#include <string>
+#include <boost/program_options.hpp>
+#include <jiffy/client/jiffy_client.h>
+#include <jiffy/utils/logger.h>
+#include <jiffy/utils/signal_handling.h>
+#include <jiffy/utils/time_utils.h>
+#include "jiffy/storage/shared_log/shared_log_defs.h"
+#include "jiffy/storage/shared_log/shared_log_partition.h"
+#include <memkind.h>
+
+using namespace ::jiffy::client;
+using namespace ::jiffy::directory;
+using namespace ::jiffy::storage;
+using namespace ::jiffy::utils;
+
+using namespace ::apache::thrift;
+
+
+TEST_CASE("shared_log_performance_test", "[write][read][performance]") {
+    std::string address = "127.0.0.1";
+    int service_port = 9090;
+    int lease_port = 9091;
+    int num_blocks = 1;
+    int chain_length = 1;
+    int num_ops = 100000;
+    int data_size = 1024;
+    
+    std::string path = "/tmp";
+    std::string backing_path = "local://tmp";
+    // Output all the configuration parameters:
+    LOG(log_level::info) << "host: " << address;
+    LOG(log_level::info) << "service-port: " << service_port;
+    LOG(log_level::info) << "lease-port: " << lease_port;
+    LOG(log_level::info) << "num-blocks: " << num_blocks;
+    LOG(log_level::info) << "chain-length: " << chain_length;
+    LOG(log_level::info) << "num-ops: " << num_ops;
+    LOG(log_level::info) << "data-size: " << data_size;
+    LOG(log_level::info) << "path: " << path;
+    LOG(log_level::info) << "backing-path: " << backing_path;
+  
+
+    struct memkind* pmem_kind = nullptr;
+    std::string pmem_path = "/media/pmem0/shijie"; 
+    std::string memory_mode = "PMEM";
+    size_t err = memkind_create_pmem(pmem_path.c_str(),0,&pmem_kind);
+    if(err) {
+        char error_message[MEMKIND_ERROR_MESSAGE_SIZE];
+        memkind_error_message(err, error_message, MEMKIND_ERROR_MESSAGE_SIZE);
+        fprintf(stderr, "%s\n", error_message);
+    }
+    size_t capacity = 134217728;
+    block_memory_manager manager(capacity, memory_mode, pmem_kind);
+    shared_log_partition block(&manager);
+    std::size_t offset = 0;
+    std::string data_ (data_size, 'x');
+
+    auto bench_begin = time_utils::now_us();
+    uint64_t tot_time = 0;
+
+    for (int i = 0; i < num_ops; ++i) {
+        response resp;
+        block.write(resp, {"write", std::to_string(i), std::to_string(offset), std::to_string(i)+"_data", std::to_string(i)+"_stream"});
+        offset += (12 + 2 * std::to_string(i).size());
+    }
+	auto bench_end = time_utils::now_us();
+	tot_time = bench_end - bench_begin;
+	LOG(log_level::info) << "===== " << "shared_log_write" << " ======";
+	LOG(log_level::info) << "total_time: " << tot_time;
+	LOG(log_level::info) << "\t" << num_ops << " requests completed in " << tot_time
+											<< " us";
+	LOG(log_level::info) << "\t" << data_size << " payload";
+	LOG(log_level::info) << "\tThroughput: " << num_ops * 1E3 / tot_time << " requests per microsecond";
+
+    bench_begin = time_utils::now_us();
+    tot_time = 0;
+    for (int i = 0; i < num_ops; ++i) {
+        response resp;
+        block.scan(resp, {"scan", std::to_string(i), std::to_string(i), std::to_string(i)+"_stream"});
+    }
+	bench_end = time_utils::now_us();
+	tot_time = bench_end - bench_begin;
+	LOG(log_level::info) << "===== " << "shared_log_scan" << " ======";
+	LOG(log_level::info) << "total_time: " << tot_time;
+	LOG(log_level::info) << "\t" << num_ops << " requests completed in " << tot_time
+											<< " us";
+	LOG(log_level::info) << "\t" << data_size << " payload";
+	LOG(log_level::info) << "\tThroughput: " << num_ops * 1E3 / tot_time << " requests per microsecond";
+    
+    bench_begin = time_utils::now_us();
+    tot_time = 0;
+    for (int i = 0; i < num_ops; ++i) {
+        response resp;
+        block.trim(resp, {"trim", std::to_string(i), std::to_string(i)});
+    }
+	bench_end = time_utils::now_us();
+	tot_time = bench_end - bench_begin;
+	LOG(log_level::info) << "===== " << "shared_log_trim" << " ======";
+	LOG(log_level::info) << "total_time: " << tot_time;
+	LOG(log_level::info) << "\t" << num_ops << " requests completed in " << tot_time
+											<< " us";
+	LOG(log_level::info) << "\t" << data_size << " payload";
+	LOG(log_level::info) << "\tThroughput: " << num_ops * 1E3 / tot_time << " requests per microsecond";
+    
+    
+    // memkind_destroy_kind(pmem_kind);
+}
