7b86dbda1192e2dbc6f5c5de381cda7d3766aeed
zhangshijie <1791473127@qq.com>
2020-11-20 17:39:57 +0800

    a

--- libjiffy/test/shared_log_performance_test.cpp
+++ libjiffy/test/shared_log_performance_test.cpp
@@ -81,7 +81,7 @@ TEST_CASE("shared_log_performance_test", "[write][read][performance]") {
         tot_time = 0;
         for (int j = 0; j < num_ops/scan_size; j+= scan_size){
             response resp;
-            std::vector<std::string> scan_args = {"scan", std::to_string(j), std::to_string(j + scan_size - 1), std::to_string(j)+"_stream"};
+            std::vector<std::string> scan_args = {"scan", std::to_string(j), std::to_string(j + scan_size - 1), std::to_string(j)+"_stream", std::to_string(j+1)+"_stream"};
             block.scan(resp, scan_args);
         }
         
