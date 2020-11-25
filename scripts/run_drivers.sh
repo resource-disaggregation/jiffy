fb0b12af3bded16ea460de9012f9425920a2844b
Midhul <midhul.v@gmail.com>
2020-11-25 12:12:01 -0500

    update

--- scripts/run_drivers.sh
+++ scripts/run_drivers.sh
@@ -7,9 +7,19 @@ trace_file=$5
 block_size=$6
 backing_path=$7
 num_threads=$8
-tenant_ids=('92', '45', '14', '79', '93', '1', '54', '96', '44', '76')
+tenant_ids=('92' '45' '14' '79' '93' '1' '54' '96' '44' '76')
+
+pids=()
 
 for tenant in "${tenant_ids[@]}"
 do
     python3 -u driver.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
+    pids+=($!);
+    echo "Launched tenant$tenant";
 done
+
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+echo "Done"
