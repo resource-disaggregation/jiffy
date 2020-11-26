85e61448216f15232b61fd99e09875f296b19e64
zhangshijie <1791473127@qq.com>
2020-11-26 18:48:08 +0800

    a

--- libjiffy/src/jiffy/storage/shared_log/shared_log_partition.cpp
+++ libjiffy/src/jiffy/storage/shared_log/shared_log_partition.cpp
@@ -71,8 +71,8 @@ void shared_log_partition::scan(response &_return, const arg_list &args) {
   if (args.size() < 4) {
     RETURN_ERR("!args_error");
   }
-  auto start_pos = std::stoi(args[1]) - seq_no_;
-  auto end_pos = std::stoi(args[2]) - seq_no_;
+  auto start_pos = std::stoi(args[1]) - seq_no;
+  auto end_pos = std::stoi(args[2]) - seq_no;
   if (end_pos > log_info_.size()) end_pos = log_info_.size() - 1;
   std::vector<std::string> logical_streams = {}; 
   for (int i = 3; i < args.size(); i++){
@@ -117,8 +117,8 @@ void shared_log_partition::trim(response &_return, const arg_list &args) {
   if (log_info_.size() == 0) {
     RETURN_OK(0);
   }
-  auto start_pos = std::stoi(args[1]) - seq_no_;
-  auto end_pos = std::stoi(args[2]) - seq_no_;
+  auto start_pos = std::stoi(args[1]) - seq_no;
+  auto end_pos = std::stoi(args[2]) - seq_no;
   if (start_pos < 0 || start_pos >= log_info_.size() || end_pos < 0 || end_pos < start_pos) throw std::invalid_argument("trim position invalid");
   if (end_pos > log_info_.size()) end_pos = log_info_.size() - 1;
   int trimmed_length = 0;
@@ -126,7 +126,11 @@ void shared_log_partition::trim(response &_return, const arg_list &args) {
     auto info_set = log_info_[i];
     if (info_set[0] == -1) continue;
     log_info_[i][0] = -1; // make the log entry invalid
+    for (int j = 1; j < info_set.size(); j++){
+      trimmed_length += info_set[j];
+    }
   }
+  
   RETURN_OK(std::to_string(trimmed_length));
 }
 
