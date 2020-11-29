000e5ed671e09bf6fd0d3040cf2f978091b4fcf3
Midhul <midhul.v@gmail.com>
2020-11-29 11:13:59 -0500

    Scripts to collect stats

--- scripts/collect_servers.sh
+++ scripts/collect_servers.sh
@@ -1,7 +1,7 @@
 config=$1
 
 echo "TODO: Fix util computation"
-avg_util=$(cat ~/karma-eval/$config.dir.log | grep -i 'utilization' | head -n $((900*100)) awk '{sum += $6} END {print sum}')
+avg_util=$(cat ~/karma-eval/$config.dir.log | grep -i 'utilization' | head -n $((900*100)) | awk '{sum += $6} END {print sum}')
 
 sync_latency=$(cat ~/karma-eval/$config.storage*.log | grep 'dirty block in' | awk '{sum += $10} END {print sum/NR}')
 num_syncs=$(cat ~/karma-eval/$config.storage*.log | grep 'dirty block in' | wc -l)
