c86f1198088f41034ccd0a8446cb239a077a92e4
Midhul <midhul.v@gmail.com>
2020-11-29 10:14:30 -0500

    Updated tcp config

--- tcp_settings.sh
+++ tcp_settings.sh
@@ -6,14 +6,14 @@ ifconfig enp126s0 txqueuelen 5000
 echo "/sbin/ifconfig enp126s0 txqueuelen 5000" >> /etc/rc.local
 
 
-sysctl net.core.somaxconn = 8096
-sysctl net.core.netdev_max_backlog = 5000
-sysctl net.core.rmem_max = 16777216
-sysctl net.core.wmem_max = 16777216
-sysctl net.ipv4.tcp_wmem = 4096 12582912 16777216
-sysctl net.ipv4.tcp_rmem = 4096 12582912 16777216
-sysctl net.ipv4.tcp_max_syn_backlog = 8096
-sysctl net.ipv4.tcp_slow_start_after_idle = 0
-sysctl net.ipv4.tcp_tw_reuse = 1
+sysctl net.core.somaxconn=8096
+sysctl net.core.netdev_max_backlog=5000
+sysctl net.core.rmem_max=16777216
+sysctl net.core.wmem_max=16777216
+sysctl net.ipv4.tcp_wmem="4096 12582912 16777216"
+sysctl net.ipv4.tcp_rmem="4096 12582912 16777216"
+sysctl net.ipv4.tcp_max_syn_backlog=8096
+sysctl net.ipv4.tcp_slow_start_after_idle=0
+sysctl net.ipv4.tcp_tw_reuse=1
 
 ulimit -n 500000
