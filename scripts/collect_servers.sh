26512cbf75c0440b93597cd425749cfabb77410c
Midhul <midhul.v@gmail.com>
2020-11-30 16:14:53 -0500

    Updated collect script

--- scripts/collect_servers.sh
+++ scripts/collect_servers.sh
@@ -1,8 +1,10 @@
 config=$1
+num_epochs=$2
+num_blocks=$3
 
-echo "TODO: Fix util computation"
-avg_util=$(cat ~/karma-eval/$config.dir.log | grep -i 'utilization' | head -n $((900*100)) | awk '{sum += $6} END {print sum/NR}')
+# echo "TODO: Fix util computation"
 
+avg_util=$(cat ~/karma-eval/$config.dir.log | grep -i 'epoch used blocks' | head -n $num_epochs | awk -v n=$num_blocks '{sum += $8} END {print (sum/NR)/n}')
 sync_latency=$(cat ~/karma-eval/$config.storage*.log | grep 'dirty block in' | awk '{sum += $10} END {print sum/NR}')
 num_syncs=$(cat ~/karma-eval/$config.storage*.log | grep 'dirty block in' | wc -l)
 
