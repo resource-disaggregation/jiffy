329d7c8e3951de770b65a43b7983247ec4c24005
zhangshijie <1791473127@qq.com>
2020-12-04 21:51:48 +0800

    a

--- benchmark/src/shared_log_benchmark.cpp
+++ benchmark/src/shared_log_benchmark.cpp
@@ -167,8 +167,8 @@ int main() {
   int num_ops = 100000;
   int data_size = 1280;
   std::vector<std::string> op_type_set;
-  op_type_set.push_back("write");
-  op_type_set.push_back("scan");
+  // op_type_set.push_back("write");
+  // op_type_set.push_back("scan");
   op_type_set.push_back("trim");
   std::string path = "/tmp";
   std::string backing_path = "local://tmp";
