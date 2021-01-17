f0a93743abb68007f567e5b76f7c02795b643ca3
zhangshijie <1791473127@qq.com>
2021-01-17 16:00:23 +0800

    minor fix

--- libjiffy/test/chain_replication_test.cpp
+++ libjiffy/test/chain_replication_test.cpp
@@ -32,7 +32,7 @@ TEST_CASE("chain_replication_no_failure_test", "[put][get]") {
                                                   STORAGE_SERVICE_PORT_N(i),
                                                   STORAGE_MANAGEMENT_PORT_N(i));
     alloc->add_blocks(block_names[i]);
-    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]);
+    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]).blocks;
 
     management_servers[i] = storage_management_server::create(blocks[i], HOST, STORAGE_MANAGEMENT_PORT_N(i));
     server_threads.emplace_back([i, &management_servers] { management_servers[i]->serve(); });
@@ -116,7 +116,7 @@ TEST_CASE("chain_replication_head_failure_test", "[put][get]") {
                                                   STORAGE_SERVICE_PORT_N(i),
                                                   STORAGE_MANAGEMENT_PORT_N(i));
     alloc->add_blocks(block_names[i]);
-    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]);
+    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]).blocks;
 
     management_servers[i] = storage_management_server::create(blocks[i], HOST, STORAGE_MANAGEMENT_PORT_N(i));
     server_threads.emplace_back([i, &management_servers] { management_servers[i]->serve(); });
@@ -210,7 +210,7 @@ TEST_CASE("chain_replication_mid_failure_test", "[put][get]") {
                                                   STORAGE_SERVICE_PORT_N(i),
                                                   STORAGE_MANAGEMENT_PORT_N(i));
     alloc->add_blocks(block_names[i]);
-    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]);
+    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]).blocks;
 
     management_servers[i] = storage_management_server::create(blocks[i], HOST, STORAGE_MANAGEMENT_PORT_N(i));
     server_threads.emplace_back([i, &management_servers] { management_servers[i]->serve(); });
@@ -304,7 +304,7 @@ TEST_CASE("chain_replication_tail_failure_test", "[put][get]") {
                                                   STORAGE_SERVICE_PORT_N(i),
                                                   STORAGE_MANAGEMENT_PORT_N(i));
     alloc->add_blocks(block_names[i]);
-    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]);
+    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]).blocks;
 
     management_servers[i] = storage_management_server::create(blocks[i], HOST, STORAGE_MANAGEMENT_PORT_N(i));
     server_threads.emplace_back([i, &management_servers] { management_servers[i]->serve(); });
@@ -398,7 +398,7 @@ TEST_CASE("chain_replication_add_block_test", "[put][get]") {
                                                   STORAGE_SERVICE_PORT_N(i),
                                                   STORAGE_MANAGEMENT_PORT_N(i));
     alloc->add_blocks(block_names[i]);
-    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]);
+    blocks[i] = test_utils::init_hash_table_blocks(block_names[i]).blocks;
 
     management_servers[i] = storage_management_server::create(blocks[i], HOST, STORAGE_MANAGEMENT_PORT_N(i));
     server_threads.emplace_back([i, &management_servers] { management_servers[i]->serve(); });
