91c80ba8652a6ffc23b503868f266f90f828723a
hesuyouren <1791473127@qq.com>
2021-01-22 22:47:37 +0800

    remove catching exception

--- libjiffy/src/jiffy/persistent/local/local_store.cpp
+++ libjiffy/src/jiffy/persistent/local/local_store.cpp
@@ -12,23 +12,12 @@ void local_store::write(const storage::locked_hash_table_type &table, const std:
   size_t found = out_path.find_last_of("/\\");
   auto dir = out_path.substr(0, found);
   directory_utils::create_directory(dir);
-  try {
-    serde()->serialize(table, out_path);
-  }
-  catch(std::exception& err) {
-    std::cout<<err.what()<<std::endl;
-  }
-  
+  serde()->serialize(table, out_path);
   out->close();
 }
 
 void local_store::read(const std::string &in_path, storage::locked_hash_table_type &table) {
-  try {
-    serde()->deserialize(table, in_path);
-  }
-  catch (...) {
-    std::cout << "catch (...)" << std::endl;
-  }
+  serde()->deserialize(table, in_path);
   in->close();
 }
 
